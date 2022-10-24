d886583f0b112a25f5d52cebd45193cbe93b10cf
Medici <107539009+taholton748@users.noreply.github.com>
2022-10-24 16:13:30 -0600

    added content and associations to models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category);
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {through: ProductTag});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {through: ProductTag});
 
 module.exports = {
   Product,
